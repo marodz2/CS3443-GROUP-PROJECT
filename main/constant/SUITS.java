797a5624e541d7bff9d90475ec5657fb008f8834
marodz2 <80734263+marodz2@users.noreply.github.com>
2021-04-29 21:43:30 -0600

    Delete main directory

--- main/constant/SUITS.java
+++
@@ -1,7 +0,0 @@
-package main.constant;
-
-//Alex Rodriguez (crm392) CS 3443 Group Project
-// Creates Suits for Cards
-public enum SUITS {
-    CLUBS, DIAMONDS, SPADES, HEARTS
-}
--- main/controller/User.java
+++
@@ -1,7 +0,0 @@
-package main.controller;
-
-//Alex Rodriguez (crm392) CS 3443 Group Project
-
-public class User extends Player {
-
-}
