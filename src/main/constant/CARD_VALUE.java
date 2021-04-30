87752391e942a6f05d0d090caa5ef1056cfcd877
marodz2 <80734263+marodz2@users.noreply.github.com>
2021-04-29 21:54:52 -0600

    Add files via upload

---
+++ src/main/constant/CARD_VALUE.java
@@ -0,0 +1,8 @@
+package main.constant;
+
+//Alex Rodriguez (crm392) CS 3443 Group Project
+//Sets Card Values
+public enum CARD_VALUE {
+    
+    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE, 
+}
---
+++ src/main/constant/SUITS.java
@@ -0,0 +1,7 @@
+package main.constant;
+
+//Alex Rodriguez (crm392) CS 3443 Group Project
+// Creates Suits for Cards
+public enum SUITS {
+    CLUBS, DIAMONDS, SPADES, HEARTS
+}
